ed923726bcb2c7fb14b470536ae4b05f1774393e
RudraShukla02 <imrudrashukla02@gmail.com>
2026-08-19 12:30:59 +0530

    Add files via upload

---
+++ FIRST.C
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+ int a,b,ad,su,mu,div;
+ printf("Enter 2 values");
+ scanf("%d%d",&a,&b);
+ ad=a+b;
+ su=a-b;
+ mu=a*b;
+ div=a/b;
+ printf("sum=%d\ndiff=%d\nmultiply=%d\ndiv=%d",ad,su,mu,div);
+ return 0;
+}
